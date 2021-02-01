9481c92ad8cb15fd65dcb7602c62f8af23ce0f81
GaahNoel <ga.noel@outlook.com>
2021-02-01 17:56:05 -0300

    feat: começo do frontend

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title> Explore repositórios no Github </Title>
+      <Form action="">
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/61170183?s=460&v=4"
+            alt="Gabriel"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>blablabla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/61170183?s=460&v=4"
+            alt="Gabriel"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>blablabla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/61170183?s=460&v=4"
+            alt="Gabriel"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>blablabla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/61170183?s=460&v=4"
+            alt="Gabriel"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>blablabla</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
